c794c21bfc79b683f483b0b3021d9d2569bab02b
Sai Chandan Mohanty <saichandan68@gmail.com>
2023-07-18 16:37:23 +0530

    api url updated

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
-import { v4 as uuidv4 } from "uuid";
 import { EditTodoForm } from "./EditTodoForm";
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:5000/items/";
+const BACKEND_URL = "https://sviet-api.onrender.com/items";
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
